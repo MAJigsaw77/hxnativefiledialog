08ec6ef1e4e5c3a4ec2c30e841f7237aed3aa9c1
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-02 21:52:05 +0300

    Update Main.hx

--- samples/opendialog/Main.hx
+++ samples/opendialog/Main.hx
@@ -7,14 +7,21 @@ class Main
 {
 	public static function main():Void
 	{
-		var outPath:NFDCharStar_T = null;
+		var outPath:cpp.RawPointer<NFDCharStar_T> = null;
 
 		var result:NFDResult_T = NFD.OpenDialog("png,jpg;pdf", null, cpp.RawPointer.addressOf(outPath));
 
 		switch (result)
 		{
 			case NFD_OKAY:
-				Sys.println('Success! $outPath');
+				if (outPath != null)
+				{
+					Sys.println('Success!');
+					Sys.println(cast(outPath, String));
+					NFD.Free(outPath);
+				}
+				else
+					Sys.println('Success, but outPath is null.');
 			case NFD_CANCEL:
 				Sys.println('User pressed cancel.');
 			default:
