5bcc9aaf6ee7dc12739379c943672a5f940d48c3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-02 21:44:21 +0300

    Update NFD.hx

--- hxnativefiledialog/NFD.hx
+++ hxnativefiledialog/NFD.hx
@@ -5,40 +5,92 @@ package hxnativefiledialog;
 #end
 import hxnativefiledialog.Types;
 
+/**
+ * This class provides static methods to interact with the Native File Dialog (NFD) library.
+ * It allows for the creation and management of file and folder dialogs, as well as handling
+ * the retrieval of selected paths and error messages.
+ */
 @:buildXml('<include name="${haxelib:hxnativefiledialog}/project/Build.xml" />')
 @:include('nfd.h')
 @:unreflective
 extern class NFD
 {
-	/* Single file open dialog */
+	/**
+	 * Open a single file dialog.
+	 *
+	 * @param filterList A semicolon-separated list of file filters, e.g., "png;jpg".
+	 * @param defaultPath The default path for the dialog to open in.
+	 * @param outPath Pointer to the output path selected by the user.
+	 * @return Result of the operation (NFD_ERROR, NFD_OKAY, NFD_CANCEL).
+	 */
 	@:native('NFD_OpenDialog')
 	static function OpenDialog(filterList:NFDConstCharStar_T, defaultPath:NFDConstCharStar_T, outPath:cpp.RawPointer<NFDCharStar_T>):NFDResult_T;
 
-	/* Multiple file open dialog */
+	/**
+	 * Open a multiple file selection dialog.
+	 *
+	 * @param filterList A semicolon-separated list of file filters.
+	 * @param defaultPath The default path for the dialog to open in.
+	 * @param outPath Pointer to the output paths selected by the user.
+	 * @return Result of the operation (NFD_ERROR, NFD_OKAY, NFD_CANCEL).
+	 */
 	@:native('NFD_OpenDialogMultiple')
 	static function OpenDialogMultiple(filterList:NFDConstCharStar_T, defaultPath:NFDConstCharStar_T, outPath:cpp.RawPointer<NFDPathSet_T>):NFDResult_T;
 
-	/* Save dialog */
+	/**
+	 * Open a save file dialog.
+	 *
+	 * @param filterList A semicolon-separated list of file filters.
+	 * @param defaultPath The default path for the dialog to open in.
+	 * @param outPath Pointer to the output path selected by the user.
+	 * @return Result of the operation (NFD_ERROR, NFD_OKAY, NFD_CANCEL).
+	 */
 	@:native('NFD_SaveDialog')
 	static function SaveDialog(filterList:NFDConstCharStar_T, defaultPath:NFDConstCharStar_T, outPath:cpp.RawPointer<NFDCharStar_T>):NFDResult_T;
 
-	/* Select folder dialog */
+	/**
+	 * Open a folder selection dialog.
+	 *
+	 * @param defaultPath The default path for the dialog to open in.
+	 * @param outPath Pointer to the output path selected by the user.
+	 * @return Result of the operation (NFD_ERROR, NFD_OKAY, NFD_CANCEL).
+	 */
 	@:native('NFD_PickFolder')
 	static function PickFolder(defaultPath:NFDConstCharStar_T, outPath:cpp.RawPointer<NFDCharStar_T>):NFDResult_T;
 
-	/* Get last error -- set when nfdresult_t returns NFD_ERROR */
+	/**
+	 * Get the last error message.
+	 * This function should be called if a function returns NFD_ERROR.
+	 *
+	 * @return The last error message.
+	 */
 	@:native('NFD_GetError')
 	static function GetError():NFDConstCharStar_T;
 
-	/* Get the number of entries stored in pathSet */
+	/**
+	 * Get the number of entries in a path set.
+	 *
+	 * @param pathSet Pointer to the path set.
+	 * @return The number of paths in the path set.
+	 */
 	@:native('NFD_PathSet_GetCount')
 	static function PathSet_GetCount(pathSet:cpp.RawConstPointer<NFDPathSet_T>):cpp.SizeT;
 
-	/* Get the UTF-8 path at offset index */
+	/**
+	 * Get a path from a path set by index.
+	 *
+	 * @param pathSet Pointer to the path set.
+	 * @param index The index of the path to retrieve.
+	 * @return The path at the specified index.
+	 */
 	@:native('NFD_PathSet_GetPath')
 	static function PathSet_GetPath(pathSet:cpp.RawConstPointer<NFDPathSet_T>, index:cpp.SizeT):NFDCharStar_T;
 
-	/* Free the pathSet */
+	/**
+	 * Free the memory used by a path set.
+	 *
+	 * @param pathSet Pointer to the path set to free.
+	 */
 	@:native('NFD_PathSet_Free')
 	static function PathSet_Free(pathSet:cpp.RawPointer<NFDPathSet_T>):Void;
 }
